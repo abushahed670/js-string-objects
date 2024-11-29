50a7e0cd71baf2df06c44e659e2912bdf090e38a
abushahed670 <abu.shahed670@gmail.com>
2024-11-30 01:28:16 +0600

    reverse added

---
+++ reverse.js
@@ -0,0 +1,24 @@
+const sentence = 'I am learning web dev.';
+//one way to reverse
+for(const letter of sentence){
+    console.log(letter)
+// }
+
+// another  way 
+let reverse = '';
+for(const letter of sentence){
+    reverse = letter + reverse;
+}
+console.log(reverse);
+
+// another way to reverse for loop 
+
+let rev = '';
+for(let i = 0;i <sentence.length; i++){
+    // console.log(i);
+    // console.log(sentence[i]);
+    const letter = sentence[i];
+    rev = letter + rev;
+
+}
+console.log(rev);

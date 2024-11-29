015c3e460de40d79ae2d70bfc26f762111525b6f
abushahed670 <abu.shahed670@gmail.com>
2024-11-29 23:42:32 +0600

    concat added

---
+++ concat.js
@@ -0,0 +1,12 @@
+const first = 'Abid';
+const second = 'Nabid';
+
+// const fullname = first + second;
+// console.log(fullname);
+
+// const fullname = first + ' ' + second;
+// console.log(fullname);
+
+
+const fullName2 = first.concat(' ').concat(second)
+console.log(fullName2);

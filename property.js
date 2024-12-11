7714e1058dbafca328969156a15c21afae22f91a
abushahed670 <abu.shahed670@gmail.com>
2024-12-11 23:04:08 +0600

    property added

---
+++ property.js
@@ -0,0 +1,13 @@
+const person ={
+    name: 'kamal',
+    age: 25,
+    profession: 'developer',
+    salary: 25000,
+    married: true
+}
+
+// console.log(person);
+console.log(person.name);
+
+const income = person.salary;
+console.log(income)

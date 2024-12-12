17b414b853a07c854760ad82d568157cc54fcb83
abushahed670 <abu.shahed670@gmail.com>
2024-12-13 01:05:38 +0600

    nested added

---
+++ nested.js
@@ -0,0 +1,17 @@
+const college ={
+    name: 'vnc',
+    class: [11, 12,],
+    events: ['science fair', 'bijoy dibos', '21 feb'],
+    unique: {
+        class: 'blue',
+        result: {
+            gpa: 5,
+            merit: 'top'
+        }
+    }
+}
+// console.log(college); 
+// console.log(college.unique.result.gpa)
+// to change gpa value
+college.unique.result.gpa = 4.5;
+console.log(college.unique.result.gpa)

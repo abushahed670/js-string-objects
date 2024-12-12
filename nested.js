39cc98d46ab9c0c763897aea2e34939d6e8be9c4
abushahed670 <abu.shahed670@gmail.com>
2024-12-13 01:08:53 +0600

    property delete

--- nested.js
+++ nested.js
@@ -14,4 +14,9 @@ const college ={
 // console.log(college.unique.result.gpa)
 // to change gpa value
 college.unique.result.gpa = 4.5;
-console.log(college.unique.result.gpa)
+console.log(college.unique.result.gpa);
+
+// to delete 
+
+delete college.class;
+console.log(college);
